5dce66dcc8f56ed2e53227ef9633b50830b662b6
Joris Granel <Joris@GRANEL>
2018-03-09 12:38:41 +0100

    Start dev PGJDBC Class

--- src/Persistence/PGJDBC.java
+++ src/Persistence/PGJDBC.java
@@ -1,6 +1,7 @@
 package Persistence;
 
 import java.util.*;
+import java.sql.*;
 
 /**
  * 
@@ -11,6 +12,11 @@ public class PGJDBC {
      * Default constructor
      */
     public PGJDBC() {
+    	
     }
-
+    public Connection CreateConnection() {
+    	Connection db = DriverManager.getConnection("jdbc:postgresql:", user, password); //Must add the db name, user and password to access the DB
+    	return db;
+    }
+    
 }
