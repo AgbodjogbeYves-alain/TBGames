021edf6db9372c20f98f0e555bfe2e828b89bf50
Mohamed Iheb Faiza <fmohamediheb@gmail.com>
2018-03-10 11:33:00 +0100

    Edit : Class User constructors getters and setters

--- src/Application/User.java
+++ src/Application/User.java
@@ -5,13 +5,46 @@ import java.util.*;
 /**
  * 
  */
-public class User {
+public class User extends Actor {
+	
+	private String zipCode ;
+	private String phoneNumber ;
 
     /**
      * Default constructor
      */
-    public User() {
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(String id, String username, String email, String password, String zipCode, String phoneNumber) {
+		super(id,username,email,password);
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public User(ArrayList<String> queryResult) {
+		super(queryResult);
+    	this.zipCode = queryResult.get(4);
+		this.phoneNumber = queryResult.get(5);
     }
+    
+	public String getZipCode() {
+		return zipCode;
+	}
 
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
 
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+    
 }
