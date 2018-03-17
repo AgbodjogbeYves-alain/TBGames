bc6cfefc5f080132691df4a3e8504ed65006fca9
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2018-03-17 14:01:33 +0100

    someChanges

--- src/presentation/userInterface/viewsHandler/loginViewHandler.java
+++ src/presentation/userInterface/viewsHandler/loginViewHandler.java
@@ -8,87 +8,71 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import presentation.MainStage;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 
 
-public class loginViewHandler extends Application {
+public class loginViewHandler extends Application{
 	
-	private Stage primaryStage;
+	private Stage loginStage;
     @FXML private TextField usernameTF;
-    @FXML private TextField passwordTF;
-    
-    public Label msg = new Label();
-    
-	@Override
-	public void start(Stage primaryStage) throws ClassNotFoundException {
-		this.primaryStage = primaryStage;
-		this.primaryStage.setResizable(false);
-        
-       
-        //initRootLayout(null);
-        
-        showView("Welcome on TBGames","loginUserView.fxml");
-	}
-	
-    /**
-     * Shows the person overview inside the root layout.
-     * @throws ClassNotFoundException 
-     */
-    public void showView(String msg, String fxml) throws ClassNotFoundException {
-        try {
-        	
-        	this.primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../../images/iconTB.png")));
-            this.primaryStage.setTitle("Welcome on TBGames!!");
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            
-            loader.setLocation(getClass().getResource("../fxml/"+fxml));
-            AnchorPane view = (AnchorPane) loader.load();
-            Scene scene = new Scene(view);
-            
-            primaryStage.setScene(scene);
-          	primaryStage.show();
-            // Set person overview into the center of root layout.
-            //rootLayout.setCenter(loginView);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }	
-    
-
+    @FXML  private TextField passwordTF;
 
 	@FXML
 	private void handlelogAction(ActionEvent event) {
 		if (ApplicationFacade.getInstance().login(usernameTF.getText(),passwordTF.getText())) {
-    		msg.setText("Accepted");
     		System.out.println("Accepted");
+    		FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainStage.class.getResource("userInterface/fxml/loginUserView.fxml"));
+			AnchorPane view;
+			try {
+				view = (AnchorPane) loader.load();
+				Scene scene = new Scene(view);
+				MainStage.setStageAttribute(scene, "zz");
+				MainStage.launcher(null);
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
     	}
     	else {
-    		msg.setText("Refused");
     		System.out.println("Refused");
     	}
+		
+		
 	}
 	
 	@FXML
 	private void handlesignupAction(ActionEvent event) throws ClassNotFoundException {
-		
-		showView("Enregistrez-vous!","signUpView.fxml");
+		loginStage.close();
+		//showView("Enregistrez-vous!","managePostView.fxml");
+		System.out.println("ta mère");
 	}
     
     /**
      * Returns the main stage.
      * @return
      */
-    public Stage getPrimaryStage() {
-        return primaryStage;
+    public Stage getloginStage() {
+        return loginStage;
     }
 	
-	public static void main(String[] args) {
-		launch(args);
+    public void setStage(Stage stage) {
+    	this.loginStage = stage;
+    }
+	public static void main(String[] args) throws IOException {
+			
+        
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 }
 
