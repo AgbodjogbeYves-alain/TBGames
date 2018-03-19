9d6c2afcbd4ac75f2d5973498492736c0eec262a
GranelJ <joris.granel@etu.umontpellier.fr>
2018-03-19 10:11:50 +0100

    add SignUpUserViewhandler, edit LoginViewHandler and SignUpUserView

---
+++ src/presentation/userInterface/viewsHandler/SignUpUserViewHandler.java
@@ -0,0 +1,14 @@
+package presentation.userInterface.viewsHandler;
+
+import application.ApplicationFacade;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import presentation.MainStage;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpUserViewHandler {
+
+}
