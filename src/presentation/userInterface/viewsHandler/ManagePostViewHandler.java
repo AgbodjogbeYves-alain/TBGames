1f5cf77419c46f7e174b2db7de0fc0e0fa90aeff
Unknown <Mahe.spaenle@etu.umontpellier.fr>
2018-03-20 16:23:58 +0100

    view manage post

--- src/presentation/userInterface/viewsHandler/ManagePostViewHandler.java
+++ src/presentation/userInterface/viewsHandler/ManagePostViewHandler.java
@@ -4,12 +4,11 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class ManagePostViewHandler extends Application {
-	@FXML private TextField usernameTF;
-	@FXML private Button validateBTN;
-	@FXML private Button cancelBTN;
 	
 	@Override
 	public void start(Stage primaryStage) {
---
+++ src/presentation/userInterface/viewsHandler/PostViewHandler.java
@@ -0,0 +1,20 @@
+package presentation.userInterface.viewsHandler;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PostViewHandler {
+	@FXML private Label labelPost;
+	@FXML private Label labelDescription;
+	@FXML private Label labelItemType;
+	@FXML private Label labelItemName;
+	@FXML private Label labelPrice;
+	@FXML private Button updateBTN;
+	@FXML private Button cancelBTN;
+	@FXML private Button deleteBTN;
+	@FXML private CheckBox offerCB;
+	@FXML private CheckBox demandCB;
+}
