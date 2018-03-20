308476bacb95ef4bfc5c30e363518ec6aa0c6980
Unknown <Mahe.spaenle@etu.umontpellier.fr>
2018-03-20 18:08:34 +0100

    views

---
+++ src/presentation/userInterface/viewsHandler/UpdateTrialOfferViewHandler.java
@@ -0,0 +1,15 @@
+package presentation.userInterface.viewsHandler;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class UpdateTrialOfferViewHandler {
+	@FXML private TextField descriptionTF;
+	@FXML private TextField priceTF;
+	@FXML private TextField posttitleTF;
+	@FXML private Button cancelBTN;
+	@FXML private Button validateBTN;
+	@FXML private TextField VersionTF;
+}
