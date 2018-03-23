712365a9a61ec2141e456659553688543059806d
Unknown <thais.aurard@etu.umontpellier.fr>
2018-03-23 14:40:51 +0100

    add seeders

---
+++ db/seeders.sql
@@ -0,0 +1,31 @@
+insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+  values ('Zetsubou no ou', 'zetsujava@yopmail.com', 'truc', true, false, false, false)
+
+  insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+    values ('Kurogitsune', 'ayakajava@yopmail.com', 'truc', true, false, false, false)
+
+insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+    values ('Yue', 'moonjava@yopmail.com', 'truc', true, false, false, false)
+
+insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+    values ('Madara', 'wolfjava@yopmail.com', 'truc', true, false, false, false)
+
+insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+    values ('Gilgamesh', 'worldjava@yopmail.com', 'truc', true, false, false, false)
+
+
+
+insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+    values ('Mordred Pendragon', 'swordjava@yopmail.com', 'truc', false, false, false, true)
+
+insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+    values ('Astolfo', 'bookjava@yopmail.com', 'truc', false, false, false, true)
+
+insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+    values ('Kino', 'motorradjava@yopmail.com', 'truc', false, false, false, true)
+
+insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+    values ('Nagisa Shiota', 'korojava@yopmail.com', 'truc', false, false, false, true)
+
+insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor)
+    values ('Bolt', 'housekijava@yopmail.com', 'truc', false, false, false, true)
