0d33846b37e7ffb0933fe69e1ab679fefa208e80
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2018-03-23 15:58:44 +0100

    modif seeders

--- db/seeders.sql
+++ db/seeders.sql
@@ -1,31 +1,32 @@
 insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber)
-  values ('Zetsubou no ou', 'zetsujava@yopmail.com', 'truc', true, false, false, false, '88888', '2222222222')
+  values ('Zetsubou no ou', 'zetsujava@yopmail.com', 'truc', true, false, false, false, '88888', '2222222222');
 
   insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber)
-    values ('Kurogitsune', 'ayakajava@yopmail.com', 'truc', true, false, false, false, '88888', '2226522222')
+    values ('Kurogitsune', 'ayakajava@yopmail.com', 'truc', true, false, false, false, '88888', '2226522222');
 
 insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber)
-    values ('Yue', 'moonjava@yopmail.com', 'truc', true, false, false, false, '88888', '2226522222')
+    values ('Yue', 'moonjava@yopmail.com', 'truc', true, false, false, false, '88888', '2226522222');
 
 insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber)
-    values ('Madara', 'wolfjava@yopmail.com', 'truc', true, false, false, false, '88888', '2222265922')
+    values ('Madara', 'wolfjava@yopmail.com', 'truc', true, false, false, false, '88888', '2222265922');
 
 insert into Buyer (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber)
-    values ('Gilgamesh', 'worldjava@yopmail.com', 'truc', true, false, false, false, '88888', '4442222222')
+    values ('Gilgamesh', 'worldjava@yopmail.com', 'truc', true, false, false, false, '88888', '4442222222');
 
 
 
 insert into Editor (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber, representativeName, validation)
-    values ('Mordred Pendragon', 'swordjava@yopmail.com', 'truc', false, false, false, true, '85888', '2222226622', "Shishigo Taiga", true)
+    values ('Mordred Pendragon', 'swordjava@yopmail.com', 'truc', false, false, false, true, '85888', '2222226622', 'Shishigo Taiga', true);
 
 insert into Editor (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber, representativeName, validation)
-    values ('Astolfo', 'bookjava@yopmail.com', 'truc', false, false, false, true,'88898', '1522222222', "Yggdmillenia Celenicke", false)
+    values ('Astolfo', 'bookjava@yopmail.com', 'truc', false, false, false, true,'88898', '1522222222', 'Yggdmillenia Celenicke', false);
 
 insert into Editor (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber, representativeName, validation)
-    values ('Kino', 'motorradjava@yopmail.com', 'truc', false, false, false, true,'18888', '9512222222', "Hermes", true)
+    values ('Kino', 'motorradjava@yopmail.com', 'truc', false, false, false, true,'18888', '9512222222', 'Hermes', true);
 
 insert into Editor (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber, representativeName, validation)
-    values ('Nagisa Shiota', 'korojava@yopmail.com', 'truc', false, false, false, true,'28888', '6398522222', "Koro Sensei", true)
+    values ('Nagisa Shiota', 'korojava@yopmail.com', 'truc', false, false, false, true,'28888', '6398522222', 'Koro Sensei', true);
 
 insert into Editor (username, email, password, isBuyer, isSuperAdmin, isAdministrator, isEditor, zipCode, phoneNumber, representativeName, validation)
-    values ('Bolt', 'housekijava@yopmail.com', 'truc', false, false, false, true,'38888', '4589222222', "Diamond Class", false)
+    values ('Bolt', 'housekijava@yopmail.com', 'truc', false, false, false, true,'38888', '4589222222', 'Diamond Class', false);
+
