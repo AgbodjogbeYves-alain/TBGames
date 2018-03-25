0da1e5110bd025d8ff5f344c11eaa168f3440f88
Mohamed Iheb Faiza <fmohamediheb@gmail.com>
2018-03-25 18:09:54 +0200

    Add : Actor DAO

---
+++ src/persistence/ActorDAOPG.java
@@ -0,0 +1,13 @@
+package persistence;
+
+import application.Actor;
+
+public class ActorDAOPG extends ActorDAO {
+
+	@Override
+	public Actor getActorById(String username, String pwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
